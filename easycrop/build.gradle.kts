238f583b8b2f95feb810294c8ca1ccd28061b8c9
lmy6268 <lmy6268@gmail.com>
2024-11-11 19:36:02 +0900

    upload changes

--- easycrop/build.gradle.kts
+++ easycrop/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("android")
     id ("com.android.library")
-    id("maven-publish")
-    id("signing")
+//    id("maven-publish")
+//    id("signing")
 }
 
 val composeBomVersion : String by project
@@ -10,11 +10,11 @@ val composeCompilerVersion : String by project
 
 android {
     namespace = "com.mr0xf00.easycrop"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
+        lint.targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -25,11 +25,11 @@ android {
         compose = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     publishing {
         singleVariant("release") {
@@ -51,68 +51,68 @@ dependencies {
     androidTestImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
-fun extProperty(key: String): String {
-    return (extra.properties[key] ?: error("property $key not found in extras")) as String
-}
+//fun extProperty(key: String): String {
+//    return (extra.properties[key] ?: error("property $key not found in extras")) as String
+//}
 
-publishing {
-    val data = object {
-        val groupId = "io.github.mr0xf00"
-        val artifactId = "easycrop"
-        val version = "0.1.1"
-        private val releasesRepoUrl =
-            uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-        private val snapshotsRepoUrl =
-            uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-        val repoUrl = if (version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-    }
-
-    publications {
-        register<MavenPublication>("release") {
-            groupId = data.groupId
-            artifactId = data.artifactId
-            version = data.version
-            afterEvaluate {
-                from(components["release"])
-            }
-            pom {
-                name.set("EasyCrop")
-                description.set("Image cropper for jetpack compose")
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-                url.set("https://github.com/mr0xf00/easycrop")
-                scm {
-                    connection.set("https://github.com/mr0xf00/easycrop.git")
-                    url.set("https://github.com/mr0xf00/easycrop")
-                }
-                developers {
-                    developer {
-                        name.set("mr0xf00")
-                        email.set("mr0xf00@proton.me")
-                    }
-                }
-            }
-        }
-    }
+//publishing {
+//    val data = object {
+//        val groupId = "io.github.mr0xf00"
+//        val artifactId = "easycrop"
+//        val version = "0.1.1"
+//        private val releasesRepoUrl =
+//            uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+//        private val snapshotsRepoUrl =
+//            uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+//
+//        val repoUrl = if (version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+//    }
+//
 //    publications {
-//        repositories {
-//            maven {
-//                name = "oss"
-//                url = data.repoUrl
-//                credentials {
-//                    username = extProperty("ossrh.user")
-//                    password = extProperty("ossrh.pass")
+//        register<MavenPublication>("release") {
+//            groupId = data.groupId
+//            artifactId = data.artifactId
+//            version = data.version
+//            afterEvaluate {
+//                from(components["release"])
+//            }
+//            pom {
+//                name.set("EasyCrop")
+//                description.set("Image cropper for jetpack compose")
+//                licenses {
+//                    license {
+//                        name.set("Apache-2.0")
+//                        url.set("https://opensource.org/licenses/Apache-2.0")
+//                    }
+//                }
+//                url.set("https://github.com/mr0xf00/easycrop")
+//                scm {
+//                    connection.set("https://github.com/mr0xf00/easycrop.git")
+//                    url.set("https://github.com/mr0xf00/easycrop")
+//                }
+//                developers {
+//                    developer {
+//                        name.set("mr0xf00")
+//                        email.set("mr0xf00@proton.me")
+//                    }
 //                }
 //            }
 //        }
 //    }
-}
-
-signing {
-    sign(publishing.publications)
-}
+////    publications {
+////        repositories {
+////            maven {
+////                name = "oss"
+////                url = data.repoUrl
+////                credentials {
+////                    username = extProperty("ossrh.user")
+////                    password = extProperty("ossrh.pass")
+////                }
+////            }
+////        }
+////    }
+//}
+//
+//signing {
+//    sign(publishing.publications)
+//}
